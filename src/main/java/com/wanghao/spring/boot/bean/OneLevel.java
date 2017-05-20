2044279f61ac4490b4ad57c6503d2b636f6d3596
huahangwanghao <1179852125@qq.com>
2017-05-20 13:19:03 +0800

    时间格式化已经可以啦。 全部显示也可以啦

--- src/main/java/com/wanghao/spring/boot/bean/OneLevel.java
+++ src/main/java/com/wanghao/spring/boot/bean/OneLevel.java
@@ -52,6 +52,7 @@ public class OneLevel {
         return "OneLevel{" +
                 "oneId=" + oneId +
                 ", name='" + name + '\'' +
+                ", crtDate=" + crtDate +
                 '}';
     }
 }
