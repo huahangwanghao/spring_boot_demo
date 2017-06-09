132f71799ebea60d3c7819abc230455e27b3a9b5
wanghao2 <wanghao2@gomeholdings.com>
2017-06-09 18:50:29 +0800

    修改.

--- src/main/java/com/wanghao/spring/boot/SpringBootDemoApplication.java
+++ src/main/java/com/wanghao/spring/boot/SpringBootDemoApplication.java
@@ -3,11 +3,13 @@ package com.wanghao.spring.boot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableTransactionManagement
 @ServletComponentScan
+@EnableAsync
 public class SpringBootDemoApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/wanghao/spring/boot/controller/TestController.java
+++ src/main/java/com/wanghao/spring/boot/controller/TestController.java
@@ -19,10 +19,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.concurrent.Callable;
 
 /**
  * 控制器
@@ -146,6 +149,26 @@ public class TestController {
 
 
 
+    
+    //-----------------------------------------------使用servlet3.0的代码--------------------------
+
+
+        @RequestMapping("/async/test")
+        @ResponseBody
+        public Callable<String> callable() {
+            // 这么做的好处避免web server的连接池被长期占用而引起性能问题，
+            // 调用后生成一个非web的服务线程来处理，增加web服务器的吞吐量。
+            return new Callable<String>() {
+                @Override
+                public String call() throws Exception {
+                    Thread.sleep(3 * 1000L);
+                    return "小单 - " + System.currentTimeMillis()%9;
+                }
+            };
+        
+
+    }
+    
 
 
 
